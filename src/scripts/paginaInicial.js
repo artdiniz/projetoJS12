8db750464ac0d74e986ce9128592ae18ed5f5218
Artur Diniz Adam <artur.daz@gmail.com>
2019-03-28 20:29:14 -0300

    Coloca prefixo http se não tiver

--- src/scripts/paginaInicial.js
+++ src/scripts/paginaInicial.js
@@ -1,4 +1,9 @@
 let paginaInicial = prompt('Escolha a página inicial')
 
+
+if(paginaInicial.substring(0,7) !== 'http://' && paginaInicial.substring(0,8) !== 'https://') {
+    paginaInicial = 'http://' + paginaInicial
+}
+
 $janelaPrincipal.src = paginaInicial
 $inputEndereco.value = paginaInicial
