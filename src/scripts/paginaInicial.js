c39048550a9c2ea7d88d82bf40d24c8a854592e3
Artur Diniz Adam <artur.daz@gmail.com>
2019-03-29 18:03:13 -0300

    Pedindo página inicial apenas na primeira vez

--- src/scripts/paginaInicial.js
+++ src/scripts/paginaInicial.js
@@ -1,11 +1,16 @@
-let paginaInicial = prompt('Escolha a página inicial')
+let paginaInicial = localStorage.getItem('paginaInicial')
 
+if(!paginaInicial) {
+    paginaInicial = prompt('Escolha a página inicial')
+}
 
-if(paginaInicial) {
+if(paginaInicial) { 
     if(paginaInicial.substring(0,7) !== 'http://' && paginaInicial.substring(0,8) !== 'https://') {
         paginaInicial = 'http://' + paginaInicial
     }
     
     $janelaPrincipal.src = paginaInicial
     $inputEndereco.value = paginaInicial
+
+    localStorage.setItem('paginaInicial', paginaInicial)
 }
